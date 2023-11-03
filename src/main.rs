e4cabff39efaea3c274265ad661df6d354c7c270
metastable-void <git@yuka.h09.eu>
2023-11-03 19:12:30 +0900

    allow generic routers

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,16 @@ use std::str::FromStr;
 use hyper::server::conn::AddrStream;
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use askama::Template;
 
+
+#[derive(Template, Clone, Debug)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    router_id: String,
+    address_v4: String,
+    address_v6: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -17,11 +26,18 @@ async fn main() {
         async move {
             // let addr = addr.clone();
             Ok::<_, Infallible>(service_fn(move |_req : Request<Body>| {
-                let file = include_str!("../index.html");
+                let router_id = env::var("ROUTER_ID").unwrap_or("rv128".to_string());
+                let address_v4 = env::var("ADDRESS_V4").unwrap_or("43.228.174.128".to_string());
+                let address_v6 = env::var("ADDRESS_V6").unwrap_or("2001:df3:14c0:1128::1".to_string());
+                let index = IndexTemplate {
+                    router_id,
+                    address_v4,
+                    address_v6,
+                };
                 let res = Response::builder()
                     .status(200)
                     .header("Content-Type", "text/html")
-                    .body(Body::from(file))
+                    .body(Body::from(index.render().unwrap()))
                     .unwrap();
                 async move {
                     Ok::<_, Infallible>(res)
